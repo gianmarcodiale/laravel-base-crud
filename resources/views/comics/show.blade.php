3ca70febbc24b6325cd0714af19d2130fdf1a29e
gianmarcodiale <98177039+gianmarcodiale@users.noreply.github.com>
2022-06-16 18:56:43 +0200

    Add show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container text-center">
+        <h1 class="my-5">{{ $comic->title }}</h1>
+        <img src="{{ $comic->thumb }}" alt="" class="img-fluid" style="max-width:200px">
+        <div class="comic_info my-3">
+            <h3>{{ $comic->price }}</h3>
+            <h5>{{ $comic->series }}</h5>
+            <h6>{{ $comic->type }}</h6>
+            <p>{{ $comic->sale_date }}</p>
+            <p>{{ $comic->description }}</p>
+        </div>
+    </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">COMICS LOGO</a>
+            <a class="navbar-brand" href="{{ route('home') }}">COMIC DB</a>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
                 aria-label="Toggle navigation">
